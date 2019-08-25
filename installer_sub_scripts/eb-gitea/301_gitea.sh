0e221874c3ab5721370bde818ee24d365818b997
emrah <emrah.com@gmail.com>
2019-08-25 20:58:37 +0300

    eb-gitea: stop the installation if the download fails

--- installer_sub_scripts/eb-gitea/301_gitea.sh
+++ installer_sub_scripts/eb-gitea/301_gitea.sh
@@ -168,6 +168,7 @@ latest_dir=$(curl -s https://dl.gitea.io/gitea/ | \
 	     sed 's~\(^/\|/$\)~~g')
 latest_ver=$(echo $latest_dir | sed 's~/~-~g')
 latest_lnk="https://dl.gitea.io/$latest_dir/$latest_ver-linux-amd64"
+[[ -z "$latest_ver" ]] && exit 1
 
 mkdir -p /root/eb_store
 [[ ! -f "/root/eb_store/$latest_ver-linux-amd64" ]] && \
