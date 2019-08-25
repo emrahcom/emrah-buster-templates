7d2c915f70960683aaba8d0878583e1e4030eaf4
emrah <emrah.com@gmail.com>
2019-08-25 22:05:58 +0300

    eb-gitea: fixed the regex

--- installer_sub_scripts/eb-gitea/301_gitea.sh
+++ installer_sub_scripts/eb-gitea/301_gitea.sh
@@ -171,8 +171,8 @@ latest_lnk="https://dl.gitea.io/$latest_dir/$latest_ver-linux-amd64"
 
 [[ -z "$latest_ver" ]] && \
 [[ -n "$(find /root/eb_store -maxdepth 1 -name 'gitea-*-linux-amd64')" ]] && \
-latest_ver=$(ls -1 /root/eb_store/gitea-*-linux-amd64 | ack -o 'gitea-.*$' | \
-             ack -o '\d+\.\d+\.\d+' | tail -n1)
+latest_ver=$(ls -1 /root/eb_store/gitea-*-linux-amd64 | \
+             ack -o 'gitea-\d+\.\d+\.\d+' | tail -n1)
 
 mkdir -p /root/eb_store
 if [[ ! -f "/root/eb_store/$latest_ver-linux-amd64" ]]
