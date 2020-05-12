287306cbca2f390dc2a1f42dcc1c4c45eefe2086
emrah <emrah.com@gmail.com>
2020-05-12 18:29:20 +0300

    eb-jitsi: enable file recording

--- installer_sub_scripts/eb-jitsi/302_jibri.sh
+++ installer_sub_scripts/eb-jitsi/302_jibri.sh
@@ -148,10 +148,10 @@ sed -i 's~//\s*startVideoMuted:.*~startVideoMuted: 10,~' \
     $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
 sed -i 's~//\s*requireDisplayName:.*~requireDisplayName: true,~' \
     $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
-#sed -i 's~//\s*fileRecordingsEnabled.*~fileRecordingsEnabled: true,~' \
-#    $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
-#sed -i 's~//\s*fileRecordingsServiceSharingEnabled.*~fileRecordingsServiceSharingEnabled: true,~' \
-#    $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
+sed -i 's~//\s*fileRecordingsEnabled.*~fileRecordingsEnabled: true,~' \
+    $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
+sed -i 's~//\s*fileRecordingsServiceSharingEnabled.*~fileRecordingsServiceSharingEnabled: true,~' \
+    $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
 sed -i 's~//\s*liveStreamingEnabled:.*~liveStreamingEnabled: true,~' \
     $ROOTFS/etc/jitsi/meet/$JITSI_HOST-config.js
 sed -i "/liveStreamingEnabled:/a \\\n    hiddenDomain: 'recorder.$JITSI_HOST'," \
