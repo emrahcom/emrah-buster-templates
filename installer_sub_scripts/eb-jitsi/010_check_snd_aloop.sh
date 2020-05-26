f7a67b9a5f0556e3a03f3061b556a1d6e23d8160
emrah <emrah.com@gmail.com>
2020-05-26 09:49:27 +0300

    eb-jitsi: changed the snd_aloop check. there is no lsmod in LXC

--- installer_sub_scripts/eb-jitsi/010_check_snd_aloop.sh
+++ installer_sub_scripts/eb-jitsi/010_check_snd_aloop.sh
@@ -14,7 +14,7 @@ echo "----------------- SND_ALOOP SUPPORT CHECK -----------------"
 
 modprobe snd_aloop 2>/dev/null || true
 
-if [ -z "$(lsmod | grep snd_aloop)" ]; then
+if [ -z "$(grep snd_aloop /proc/modules)" ]; then
     cat <<EOF
 
 This kernel ($(uname -r)) does not support snd_aloop module.
