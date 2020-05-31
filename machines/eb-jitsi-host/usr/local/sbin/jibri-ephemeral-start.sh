06f51aeab5051ab58891d278148aa4f82102be37
emrah <emrah.com@gmail.com>
2020-05-31 09:41:12 +0300

    eb-jitsi: limit for the ephemeral jibri instances

--- machines/eb-jitsi-host/usr/local/sbin/jibri-ephemeral-start.sh
+++ machines/eb-jitsi-host/usr/local/sbin/jibri-ephemeral-start.sh
@@ -5,12 +5,17 @@
 # -----------------------------------------------------------------------------
 #
 # Create and run the ephimeral Jibri containers. The number of containers
-# depends on the CPU count.
+# depends on the CORES count (one Jibri instance per 2 cores) but it can not be
+# more than LIMIT.
 #
 # -----------------------------------------------------------------------------
+LIMIT=16
 CORES=$(nproc --all)
 
-for c in $(seq 2 2 $CORES); do
+(( N = LIMIT * 2 ))
+[[ $N -gt $CORES ]] && N=$CORES
+
+for c in $(seq 2 2 $N); do
     (( ID = c / 2 ))
 
     lxc-copy -n eb-jibri-template -N eb-jibri-$ID -e
