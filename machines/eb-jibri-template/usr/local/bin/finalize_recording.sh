462689b42c30c334a8ee450eaf4803691082b443
emrah <emrah.com@gmail.com>
2020-09-13 17:59:28 +0300

    eb-jibri: updated the finalize script

--- machines/eb-jibri-template/usr/local/bin/finalize_recording.sh
+++ machines/eb-jibri-template/usr/local/bin/finalize_recording.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+set -e
 
-echo ok.
+FOLDER=$1
+KEY=$(basename $FOLDER)
+VIDEO_PATH=$(find $FOLDER -name '*.mp4' | head -1)
+VIDEO_NAME=$(basename $VIDEO_PATH)
+VIDEO_NAME_KEY=$(echo $VIDEO_NAME | sed "s/.mp4/_$KEY.mp4/")
+
+[[ ! -f "$VIDEO_PATH" ]] && exit 1
+
+# write your codes here. for example:
+
+exit 0
