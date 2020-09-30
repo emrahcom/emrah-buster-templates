55bce71554086ecb7d0e03b37cf0d9eea577d565
emrah <emrah.com@gmail.com>
2020-09-30 18:15:27 +0300

    eb-jitsi: fixed the token_owner_party plugin

--- machines/eb-jitsi/usr/share/jitsi-meet/prosody-plugins/mod_token_owner_party.lua
+++ machines/eb-jitsi/usr/share/jitsi-meet/prosody-plugins/mod_token_owner_party.lua
@@ -12,14 +12,18 @@
 --
 --    /usr/share/jitsi-meet/prosody-plugins/
 --
--- 2) Enable module in your prosody config.
+-- 2) Enable module in your prosody config. If you want change the timeout sec.
 --    /etc/prosody/conf.d/meet.mydomain.com.cfg.lua
 --
 --    Component "conference.meet.mydomain.com" "muc"
 --       modules_enabled = {
+--         ...
+--         ...
 --         "token_verification";
 --         "token_affiliation";
 --         "token_owner_party";
+--       }
+--       party_check_timeout = 20
 --
 -- 3) For most scenarios you may want to disable auto-ownership on Jicofo.
 --    Add the following line to /etc/jitsi/jicofo/sip-communicator.properties
@@ -31,8 +35,8 @@
 --    systemctl restart prosody.service
 --    systemctl restart jicofo.service
 -- ----------------------------------------------------------------------------
-local TIMEOUT = 60
 local LOGLEVEL = "debug"
+local TIMEOUT = module:get_option_number("party_check_timeout", 60)
 
 local is_admin = require "core.usermanager".is_admin
 local is_healthcheck_room = module:require "util".is_healthcheck_room
@@ -48,12 +52,13 @@ end
 module:hook("muc-occupant-pre-join", function (event)
     local room, stanza = event.room, event.stanza
     local user_jid = stanza.attr.from
-    
+
     if is_healthcheck_room(room.jid) or _is_admin(user_jid) then
         module:log(LOGLEVEL, "location check, %s", user_jid)
         return
     end
 
+    -- if an owner joins, start the party
     local context_user = event.origin.jitsi_meet_context_user
     if context_user then
         if context_user["affiliation"] == "owner" then
@@ -62,6 +67,7 @@ module:hook("muc-occupant-pre-join", function (event)
         end
     end
 
+    -- if the party has not started yet, don't accept the participant
     local occupant_count = it.count(room:each_occupant())
     if occupant_count < 2 then
         module:log(LOGLEVEL, "the party has not started yet")
@@ -77,32 +83,50 @@ module:hook("muc-occupant-left", function (event)
         return
     end
 
+    -- no need to do anything for normal participant
     if room:get_affiliation(occupant.jid) ~= "owner" then
         module:log(LOGLEVEL, "a participant leaved, %s", occupant.jid)
         return
     end
+
+    -- the owner is gone, start to check the room condition
+    room:broadcast_message(st.message({ type="groupchat", from=occupant.nick })
+                           :tag("body"):text("The owner is gone"))
     module:log(LOGLEVEL, "an owner leaved, %s", occupant.jid)
 
-    for _, occ in room:each_occupant() do
-        if not _is_admin(occ.jid) then
-            if room:get_affiliation(occ.jid) == "owner" then
-                module:log(LOGLEVEL, "an owner is here, %s", occ.jid)
+    -- check if there is any other owner here
+    for _, o in room:each_occupant() do
+        if not _is_admin(o.jid) then
+            if room:get_affiliation(o.jid) == "owner" then
+                module:log(LOGLEVEL, "an owner is here, %s", o.jid)
                 return
             end
         end
     end
 
+    -- since there is no other owner, kick all participants after TIMEOUT secs
     timer.add_task(TIMEOUT, function()
-        for _, occ in room:each_occupant() do
-            if not _is_admin(occ.jid) then
-                if room:get_affiliation(occ.jid) == "owner" then
-                    module:log(LOGLEVEL, "timer, an owner is here, %s", occ.jid)
+        -- last check before kicking all participants
+	-- if the owner is returned, cancel
+        for _, o in room:each_occupant() do
+            if not _is_admin(o.jid) then
+                if room:get_affiliation(o.jid) == "owner" then
+                    module:log(LOGLEVEL, "timer, an owner is here, %s", o.jid)
                     return
                 end
             end
         end
 
+        -- kick all participants
+        for _, p in room:each_occupant() do
+            if not _is_admin(p.jid) then
+                if room:get_affiliation(p.jid) ~= "owner" then
+                    room:set_affiliation(true, p.jid, "outcast")
+                    module:log(LOGLEVEL, "timer, kick the occupant, %s", p.jid)
+                end
+            end
+        end
+
         module:log(LOGLEVEL, "the party finished")
-	room:destroy(nil, "The owner is gone")
     end)
 end)
