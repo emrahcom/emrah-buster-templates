98d8ab31deee8e953508dfd1520f4bd0dad0ed0b
emrah <emrah.com@gmail.com>
2020-10-30 12:49:12 +0300

    eb-jitsi: run the existing one if the customization script is already exist

--- installer_sub_scripts/eb-jitsi/303_jitsi_customization.sh
+++ installer_sub_scripts/eb-jitsi/303_jitsi_customization.sh
@@ -25,13 +25,16 @@ JITSI_MEET="/var/lib/lxc/eb-jitsi/rootfs/usr/share/jitsi-meet"
 
 if [[ ! -d "/root/jitsi_customization" ]]
 then
-    cp -arp jitsi_customization /root/
+    cp -arp root/jitsi_customization /root/
     cp $JITSI_MEET/images/favicon.ico /root/jitsi_customization/
     cp $JITSI_MEET/images/watermark.png /root/jitsi_customization/
 
     sed -i "s/___TURN_HOST___/$TURN_HOST/g" /root/README.md
     sed -i "s/___JITSI_HOST___/$JITSI_HOST/g" /root/README.md
     sed -i "s/___JITSI_HOST___/$JITSI_HOST/g" /root/customize.sh
+else
+    echo "The customization folder already exists."
+    echo "Running the existing one..."
 fi
 
 bash /root/jitsi_customization/customize.sh
