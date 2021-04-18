cde7ebd6133e3a205f8f96986d140baaa98fca77
emrah <emrah.com@gmail.com>
2021-04-19 00:30:41 +0300

    eb-jitsi: simplified the customization script

--- machines/eb-jitsi-host/root/jitsi_customization/customize.sh
+++ machines/eb-jitsi-host/root/jitsi_customization/customize.sh
@@ -37,44 +37,10 @@ cp $JITSI_MEET/images/watermark.svg $BACKUP/
 # -----------------------------------------------------------------------------
 # jitsi-meet config.js
 # -----------------------------------------------------------------------------
-sed -i "/^\s*p2pTestMode:/ s~false$~false,~" $JITSI_MEET_CONFIG
-sed -i '/^\s*capScreenshareBitrate:/d' $JITSI_MEET_CONFIG
-sed -i '/\/\/ capScreenshareBitrate/a \        capScreenshareBitrate: 1' \
-    $JITSI_MEET_CONFIG
-
-sed -i "/disableAudioLevels:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/disableAudioLevels:/ s~:.*~: true,~" $JITSI_MEET_CONFIG
-sed -i "/startAudioMuted:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/startAudioMuted:/ s~:.*~: 10,~" $JITSI_MEET_CONFIG
-sed -i "/startWithAudioMuted:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/startWithAudioMuted:/ s~:.*~: false,~" $JITSI_MEET_CONFIG
-
-sed -i "/startVideoMuted:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/startVideoMuted:/ s~:.*~: 10,~" $JITSI_MEET_CONFIG
 sed -i "/startWithVideoMuted:/ s~//\s*~~" $JITSI_MEET_CONFIG
 sed -i "/startWithVideoMuted:/ s~:.*~: true,~" $JITSI_MEET_CONFIG
 sed -i "/channelLastN:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/channelLastN:/ s~:.*~: 4,~" $JITSI_MEET_CONFIG
-sed -i "/requireDisplayName:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/requireDisplayName:/ s~:.*~: true,~" $JITSI_MEET_CONFIG
-sed -i "/defaultLanguage:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/defaultLanguage:/ s~:.*~: 'en',~" $JITSI_MEET_CONFIG
-sed -i "/disableInviteFunctions:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/disableInviteFunctions:/ s~:.*~: false,~" $JITSI_MEET_CONFIG
-sed -i "/doNotStoreRoom:/ s~//\s*~~" $JITSI_MEET_CONFIG
-sed -i "/doNotStoreRoom:/ s~:.*~: false,~" $JITSI_MEET_CONFIG
-
-#sed -i "/resolution:/ s~//\s*~~" $JITSI_MEET_CONFIG
-#sed -i "/resolution:/ s~:.*~: 720,~" $JITSI_MEET_CONFIG
-#sed -i "/constraints:/ s~//\s*~~" $JITSI_MEET_CONFIG
-#sed -i "/constraints:/ s~:.*~: \
-#{\
-#video: {\
-#aspectRatio: 16 / 9, \
-#height: {\
-#ideal: 720, \
-#max: 720, \
-#min: 240}}},~" $JITSI_MEET_CONFIG
+sed -i "/channelLastN:/ s~:.*~: 6,~" $JITSI_MEET_CONFIG
 
 # -----------------------------------------------------------------------------
 # jitsi-meet interface_config.js
@@ -87,35 +53,14 @@ cp $WATERMARK $JITSI_MEET/images/
 sed -i "/^\s*APP_NAME:/ s~:.*~: '$APP_NAME',~" $JITSI_MEET_INTERFACE
 sed -i "/^\s*DISABLE_JOIN_LEAVE_NOTIFICATIONS:/ s~:.*~: true,~" \
     $JITSI_MEET_INTERFACE
-sed -i "/^\s*DISABLE_VIDEO_BACKGROUND:/ s~:.*~: true,~" $JITSI_MEET_INTERFACE
 sed -i "/^\s*GENERATE_ROOMNAMES_ON_WELCOME_PAGE:/ s~:.*~: false,~" \
     $JITSI_MEET_INTERFACE
-sed -i "/^\s*HIDE_INVITE_MORE_HEADER:/ s~:.*~: false,~" $JITSI_MEET_INTERFACE
 sed -i "/^\s*JITSI_WATERMARK_LINK:/ s~:.*~: '$WATERMARK_LINK',~" \
     $JITSI_MEET_INTERFACE
-sed -i "/^\s*LANG_DETECTION:/ s~:.*~: true,~" $JITSI_MEET_INTERFACE
 sed -i "/ENFORCE_NOTIFICATION_AUTO_DISMISS_TIMEOUT:/ s~//\s*~~" \
     $JITSI_MEET_INTERFACE
 sed -i "/^\s*ENFORCE_NOTIFICATION_AUTO_DISMISS_TIMEOUT:/ s~:.*~: 5000,~" \
     $JITSI_MEET_INTERFACE
-
-sed -i "s~'videobackgroundblur'~''~" $JITSI_MEET_INTERFACE
-#sed -i "s~'embedmeeting'~''~" $JITSI_MEET_INTERFACE
-#sed -i "s~'invite'~''~" $JITSI_MEET_INTERFACE
-#sed -i "s~'feedback'~''~" $JITSI_MEET_INTERFACE
-
-# -----------------------------------------------------------------------------
-# jitsi-meet customization
-# -----------------------------------------------------------------------------
-cp $BASEDIR/chat.svg $JITSI_MEET/images/
-cp $BASEDIR/custom.css $JITSI_MEET/css/
-sed -i "/custom\.css?v=/d" $JITSI_MEET/index.html
-sed -i "/link.*stylesheet.*all.css?v=/a \
-\    <link rel=\"stylesheet\" href=\"css/custom.css?v=$JITSI_MEET_VERSION\">" \
-    $JITSI_MEET/index.html
-sed -i "/\"app\.bundle\.min\.js\",/a \
-\            \"custom.css?v=$JITSI_MEET_VERSION\"," \
-    $JITSI_MEET/index.html
 
 # -----------------------------------------------------------------------------
 # jwt
